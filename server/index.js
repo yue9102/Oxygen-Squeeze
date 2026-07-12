df4e3356fb37e46e8f11223f7c556162703db4a7
yue9102 <llliuyueeee@gmail.com>
2026-07-13 01:48:15 +0800

    docs: shorten case study route

--- server/index.js
+++ server/index.js
@@ -2,9 +2,14 @@ export default {
   async fetch(request, env) {
     const url = new URL(request.url);
 
-    if (url.pathname === "/") {
-      url.pathname = "/oxygen-squeeze-case-study.html";
-      return Response.redirect(url.toString(), 302);
+    if (url.pathname === "/" || url.pathname === "/index.html") {
+      url.pathname = "/lili.html";
+      return env.ASSETS.fetch(new Request(url.toString(), request));
+    }
+
+    if (url.pathname === "/oxygen-squeeze-case-study" || url.pathname === "/oxygen-squeeze-case-study.html") {
+      url.pathname = "/";
+      return Response.redirect(url.toString(), 301);
     }
 
     if (env && env.ASSETS && typeof env.ASSETS.fetch === "function") {
